bc3d8e45d282e3dd90634ab20a40a9a2e5636f33
monnta <User@Room111-09>
2017-11-10 12:33:44 +0700

    add Junit test for class

---
+++ LabGit/src/controller/Test.java
@@ -0,0 +1,23 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
